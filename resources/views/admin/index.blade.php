08b207cc27b7a9e19a0ef9241bb31d0cef20027a
Jonathon Wallen <jonathon@monkii.com>
2017-06-27 10:59:33 +1000

    Updated blade to include required js and css

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,29 +12,31 @@
 @section('title', 'Assets')
 
 @section('styles')
-<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/elfinder.full.css') ?>">
-<link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/theme.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/elfinder.full.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?= asset($dir . '/css/theme.css') ?>">
+    <link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
 @endsection
 
 @section('content')
-<div id="elfinder"></div>
+    <div id="elfinder"></div>
 @endsection
 
 @section('scripts')
-<script src="<?= asset($dir . '/js/elfinder.min.js') ?>"></script>
+    <script src="<?= asset($dir . '/js/elfinder.min.js') ?>"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js"></script>
 
-<script type="text/javascript" charset="utf-8">
-    // Documentation for client options:
-    // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
-    $(document).ready(function() {
-        $('#elfinder').elfinder({
-            // set your elFinder options here
-            customData: {
-                _token: '<?= csrf_token() ?>'
-            },
-            url : '<?= route("elfinder.connector") ?>',  // connector URL
-            useBrowserHistory: false // remove URL hash
+    <script type="text/javascript" charset="utf-8">
+        // Documentation for client options:
+        // https://github.com/Studio-42/elFinder/wiki/Client-configuration-options
+        $(document).ready(function() {
+            $('#elfinder').elfinder({
+                // set your elFinder options here
+                customData: {
+                    _token: '<?= csrf_token() ?>'
+                },
+                url : '<?= route("elfinder.connector") ?>',  // connector URL
+                useBrowserHistory: false // remove URL hash
+            });
         });
-    });
-</script>
+    </script>
 @endsection
